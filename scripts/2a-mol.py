6ffe24d4c28f3b955d6604af1797366306fd9ae4
Paul HIVERT <paul.hivert@ynov.com>
2018-10-23 18:58:51 +0200

    todo:sd.out

--- scripts/2a-mol.py
+++ scripts/2a-mol.py
@@ -3,27 +3,50 @@
 # Author: Paul hivert
 # Date: 23/10/2018
 
-import sys
+
 import random
-import datetime
+import time
 
 secret = random.randint(1, 100)
-infinity = 0
-big = "too big"
+success = False
+big = "too big\n"
 small = "too small"
 win = "you win"
+while success is not True:
+    f = open("2a-write.txt", "r")
 
-f = open("2a-write.txt", "r")
+    lst = f.readlines()
+    lastLine = lst[len(lst) - 1]
+    f.close()
+    f = open("2a-write.txt", "w")
+    f.write("\nbienvenue au jeu du + ou -\n")
+    f.close()
 
-lst = f.readline()
-lastLine = lst[len(lst)[-1]]
+    try:
+        lastLine == int(lastLine)
+    except Exception as e:
+            print(str(e))
+            print("écrivez votre numéro a la fin du ficher 2a-mol.py vous avez 10 secondes\n")
+            time.sleep(10)
+            f = open("2a-write.txt", "r")
+            lst = f.readlines()
+            f.close()
+            lastLine = lst[len(lst) - 1]
 
-f.write("bienvenue au jeu du + ou -")
+    lastLine = int(lastLine)
 
+    if lastLine == secret:
+        with open("2a-write.txt", "w") as f:
+            f.write(win)
+            f.write("\n")
+        success = True
 
-if lastLine == secret:
-    f.write(win, "a")
-elif lastLine < secret:
-    f.write(small, "a")
-elif lastLine > secret:
-    f.write(big, "a")
+    elif lastLine < secret:
+        with open("2a-write.txt", "w") as f:
+            f.write(small)
+            f.write("\n")
+
+    elif lastLine > secret:
+        with open("2a-write.txt", "w") as f:
+            f.write(big)
+            f.write("\n")
